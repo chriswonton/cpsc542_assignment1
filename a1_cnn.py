4a2ee12ed7cd0b59e7459884714c59ce30f48265
root <root@fowler-lpw-dgx-0.chapman.edu>
2024-02-28 03:59:23 +0000

    updated models and visualizations

--- a1_cnn.py
+++ a1_cnn.py
@@ -4,11 +4,12 @@ import tensorflow as tf
 import tensorflow.keras as kb
 from tensorflow.keras.utils import image_dataset_from_directory
 from tensorflow.keras.models import load_model
-from sklearn.metrics import accuracy_score, confusion_matrix
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.layers import Input
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.utils import class_weight
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
@@ -19,20 +20,20 @@ warnings.filterwarnings('ignore')
 
 # load train and test datasets
 x_train = image_dataset_from_directory(
-    '/app/rundir/assignment1/jellyfish_dataset/Train_Test_Valid/Train',
+    '/app/rundir/cpsc542_assignment1/jellyfish_dataset/Train_Test_Valid/Train',
     image_size=(128, 128),
     label_mode='int',
     batch_size=32,
     shuffle=True)
 
 x_test = image_dataset_from_directory(
-    '/app/rundir/assignment1/jellyfish_dataset/Train_Test_Valid/test',
+    '/app/rundir/cpsc542_assignment1/jellyfish_dataset/Train_Test_Valid/test',
     image_size=(128, 128),
     label_mode='int',
     batch_size=32,
-    shuffle=True)
+    shuffle=False)
 
-# Data Augmentation
+# data Augmentation
 data_augmentation = tf.keras.Sequential([
     tf.keras.layers.experimental.preprocessing.RandomFlip('horizontal'),
     tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
@@ -46,15 +47,21 @@ def preprocess_data(x, y):
 x_train = x_train.map(preprocess_data)
 x_test = x_test.map(preprocess_data)
 
+X_train, y_train = np.concatenate([x for x, y in x_train], axis=0), np.concatenate([y for x, y in x_train], axis=0)
+X_test, y_test = np.concatenate([x for x, y in x_test], axis=0), np.concatenate([y for x, y in x_test], axis=0)
+
+# train-test split
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+
 # ----- CNN (Deep Learning Model) -----
 
-# Use MobileNetV2 as base model
+# use MobileNetV2 as base model for transfer learning
 base_model = MobileNetV2(include_top=False, input_tensor=Input(shape=(128, 128, 3)))
 
-# Freeze the layers of the base model
+# freeze the layers of the base model
 base_model.trainable = False
 
-# Add custom layers on top of MobileNetV2
+# add custom layers on top of MobileNetV2
 cnn = kb.Sequential([
     base_model,
     kb.layers.GlobalAveragePooling2D(),
@@ -64,43 +71,93 @@ cnn = kb.Sequential([
     kb.layers.Dense(6, activation='softmax')
 ])
 
-# Compile the model with an initial learning rate
+# compile the model with an initial learning rate
 initial_learning_rate = 0.001
 optimizer = tf.keras.optimizers.Adam(learning_rate=initial_learning_rate)
 cnn.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=["accuracy"])
 
-# Class Weighting
-# class_weights = {0: 1.0, 1: 1.0, 2: 1.0, 3: 1.0, 4: 1.0, 5: 1.0}
-
-# Train the model with early stopping
-early_stop = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+# train the model with early stopping
+# early_stop = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+early_stop = EarlyStopping(monitor='val_loss', patience=5)
 history = cnn.fit(x_train, batch_size=32, epochs=100, validation_data=x_test)
 
-# Evaluate the model on the test set
-# test_loss, test_accuracy = cnn.evaluate(x_test)
-# print("CNN Accuracy:", test_accuracy)
-
-# Plot training history
+# plot training history
 plt.plot(history.history['accuracy'], label='Training Accuracy')
 plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.legend()
-plt.savefig('accuracy_cnn')
+plt.savefig('visualizations/cnn/accuracy_cnn')
 
 # cnn.save('cnn_trained.h5')
 # cnn = load_model('cnn_trained.h5')
 
-y_pred = cnn.predict(x_test).argmax(axis=1)
-y_true = np.concatenate([y for x, y in x_test], axis=0).argmax(axis=1)
+# confusion matrices
+classes = ['Barrel', 'Blue', 'Compass', 'Lion\'s Mane', 'Mauve Stinger', 'Moon']
 
-# accuracy = accuracy_score(y_true, y_pred)
-# print('CNN Accuracy:', accuracy)
+# training confusion matrix
+y_pred_probs_train = cnn.predict(X_train)
+y_pred_labels_train = np.argmax(y_pred_probs_train, axis=1)
+y_true = np.argmax(y_train, axis=1)
 
-cm = confusion_matrix(y_true, y_pred)
-plt.figure(figsize=(10, 8))
-sns.heatmap(cm, annot=True, cmap='Blues', fmt='g', cbar=False)
+cm = confusion_matrix(y_true, y_pred_labels_train)
+weights = class_weight.compute_sample_weight('balanced', y_true)
+cm_norm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+plt.figure(figsize=(10,8))
+sns.heatmap(cm_norm, annot=True, fmt='.2f', cmap='Blues', xticklabels=classes, yticklabels=classes)
+plt.title('CNN Training Confusion Matrix')
 plt.xlabel('Predicted')
 plt.ylabel('True')
-plt.title('CNN Confusion Matrix for Test Dataset')
-plt.savefig('cm_cnn_test.png')
+plt.savefig('visualizations/cnn/cm_train_cnn.png')
+
+# test confusion matrix
+y_pred_probs_test = cnn.predict(X_test)
+y_pred_labels_test = np.argmax(y_pred_probs_test, axis=1)
+y_true = np.argmax(y_test, axis=1)
+
+cm = confusion_matrix(y_true, y_pred_labels_test)
+weights = class_weight.compute_sample_weight('balanced', y_true)
+cm_norm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+plt.figure(figsize=(10,8))
+sns.heatmap(cm_norm, annot=True, fmt='.2f', cmap='Blues', xticklabels=classes, yticklabels=classes)
+plt.title('CNN Test Confusion Matrix')
+plt.xlabel('Predicted')
+plt.ylabel('True')
+plt.savefig('visualizations/cnn/cm_test_cnn.png')
+
+label_mapping = {
+    0: 'Barrel',
+    1: 'Blue',
+    2: 'Compass',
+    3: 'Lion\'s Mane',
+    4: 'Mauve Stinger',
+    5: 'Moon'
+}
+
+# get some test images
+images, labels = next(iter(x_test))
+images = images * np.max(images) * 255
+
+# make predictions
+predictions = cnn.predict(images)
+
+# visualize predictions
+fig, axes = plt.subplots(nrows=5, ncols=5, figsize=(15, 15),
+    gridspec_kw={'hspace': 0.5, 'wspace': 0.5})
+
+for i, ax in enumerate(axes.flat):
+    # plot image
+    ax.imshow(images[i].numpy().astype('uint8'))
+    ax.axis('off')
+
+    # set title to predicted class and true class using label mapping
+    pred_class = np.argmax(predictions[i])
+    true_class = np.argmax(labels[i])
+    pred_class_name = label_mapping[pred_class]
+    true_class_name = label_mapping[true_class]
+    ax.set_title(f"Pred: {pred_class_name}\nTrue: {true_class_name}",
+        color=("green" if pred_class == true_class else "red"))
+    
+plt.savefig('visualizations/cnn/vis_cnn.png')
--- a1_rf.py
+++ a1_rf.py
@@ -4,27 +4,29 @@ from tensorflow.keras.utils import image_dataset_from_directory
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
+from sklearn.utils import class_weight
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
 import warnings
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 warnings.filterwarnings('ignore')
 
 # load train and test datasets
 x_train = image_dataset_from_directory(
-    '/app/rundir/assignment1/jellyfish_dataset/Train_Test_Valid/Train',
+    '/app/rundir/cpsc542_assignment1/jellyfish_dataset/Train_Test_Valid/Train',
     image_size=(128, 128),
     label_mode='int',
     batch_size=32,
-    shuffle=True)
+    shuffle=False)
 
 x_test = image_dataset_from_directory(
-    '/app/rundir/assignment1/jellyfish_dataset/Train_Test_Valid/test',
+    '/app/rundir/cpsc542_assignment1/jellyfish_dataset/Train_Test_Valid/test',
     image_size=(128, 128),
     label_mode='int',
     batch_size=32,
-    shuffle=True)
+    shuffle=False)
 
 # ----- Random Forest (Non Deep Learning Model) -----
 # extract images and labels from datasets
@@ -84,17 +86,30 @@ print('Random Forest Test Accuracy: ', accuracy_test)
 print('Random Forest Model Precision: ', precision)
 print('Random Forest Model Recall: ', recall)
 
-# get true labels for test dataset
-y_true = y_test
+# confusion matrices
+classes = ['Barrel', 'Blue', 'Compass', 'Lion\'s Mane', 'Mauve Stinger', 'Moon']
 
-# confusion matrix
-cm = confusion_matrix(y_true, y_pred)
-plt.figure(figsize=(10, 8))
-sns.heatmap(cm, annot=True, cmap='Blues', fmt='g', cbar=False)
+# train confusion matrix
+cm = confusion_matrix(y_train, rf.predict(X_train_flat))
+weights = class_weight.compute_sample_weight('balanced', y_train)
+cm_norm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+plt.figure(figsize=(10,8))
+sns.heatmap(cm, annot=True, fmt='.2f', cmap='Blues', xticklabels=classes, yticklabels=classes)
+plt.title('RF Test Confusion Matrix')
 plt.xlabel('Predicted')
 plt.ylabel('True')
-plt.title('Random Forest Confusion Matrix')
-plt.savefig('cm_rf.png')
+plt.savefig('visualizations/rf/cm_train_rf.png')
+
+# test confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+weights = class_weight.compute_sample_weight('balanced', y_test)
+cm_norm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+plt.figure(figsize=(10,8))
+sns.heatmap(cm, annot=True, fmt='.2f', cmap='Blues', xticklabels=classes, yticklabels=classes)
+plt.title('RF Test Confusion Matrix')
+plt.xlabel('Predicted')
+plt.ylabel('True')
+plt.savefig('visualizations/rf/cm_test_rf.png')
 
 # visualize predictions
 label_mapping = {
@@ -120,6 +135,7 @@ for i, ax in enumerate(axes.flat):
     # set title to predicted class and true class
     pred_class = y_pred_names[i]
     true_class = y_test_names[i]
-    ax.set_title(f"Pred: {pred_class}\nTrue: {true_class}")
+    ax.set_title(f"Pred: {pred_class}\nTrue: {true_class}",
+        color=("green" if pred_class == true_class else "red"))
 
-plt.savefig('vis_rf.png')
+plt.savefig('visualizations/rf/vis_rf.png')
